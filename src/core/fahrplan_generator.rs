433b15a24fd5ade4fa5813a435bdfebb5a58672a
yxyx-github <dev_mw@vodafonemail.de>
2025-08-20 18:46:10 +0200

    wip: refactor using TypedZusi

--- src/core/fahrplan_generator.rs
+++ src/core/fahrplan_generator.rs
@@ -4,27 +4,25 @@ use crate::core::fahrplan_generator::generate_fahrplan_error::GenerateFahrplanEr
 use crate::input::fahrplan_config::FahrplanConfig;
 use crate::input::ZusiEnvironment;
 use std::path::{Path, PathBuf};
+use zusi_xml_lib::xml::zusi::fahrplan::Fahrplan;
 use zusi_xml_lib::xml::zusi::info::DateiTyp;
-use zusi_xml_lib::xml::zusi::{Zusi, ZusiValue};
+use zusi_xml_lib::xml::zusi::{TypedZusi, Zusi, ZusiValue};
 
 pub fn generate_fahrplan(env: ZusiEnvironment, config: FahrplanConfig) -> Result<(), GenerateFahrplanError> {
-    let mut zusi = read_fahrplan(config.generate_from)?;
-    let fahrplan = match zusi {
-        Zusi { value: ZusiValue::Fahrplan(ref mut fahrplan), .. } => fahrplan,
-        _ => unreachable!(), // already checked inside read_fahrplan
-    };
+    let zusi = read_fahrplan(config.generate_from)?;
 
     // TODO: modify fahrplan
 
+    let zusi: Zusi = zusi.into();
     zusi.to_xml_file_by_path(&config.generate_at).map_err(|error| (config.generate_at, error))?;
 
     todo!()
 }
 
-fn read_fahrplan<P: AsRef<Path> + Into<PathBuf>>(path: P) -> Result<Zusi, GenerateFahrplanError> {
+fn read_fahrplan<P: AsRef<Path> + Into<PathBuf>>(path: P) -> Result<TypedZusi<Fahrplan>, GenerateFahrplanError> {
     match Zusi::from_xml_file_by_path(path.as_ref()) {
         Ok(zusi @ Zusi { value: ZusiValue::Fahrplan(_), .. }) => {
-            Ok(zusi)
+            Ok(zusi.try_into().unwrap())
         }
         Ok(_) => Err(GenerateFahrplanError::FileTypeError {
             path: path.into(),
