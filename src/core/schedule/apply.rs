2fa24340d7d850ce237070b2211a3ac02ae56c23
yxyx-github <dev_mw@vodafonemail.de>
2025-08-29 17:50:04 +0200

    implement apply schedule with time fix

--- src/core/schedule/apply.rs
+++ src/core/schedule/apply.rs
@@ -1,6 +1,6 @@
-use time::{Duration, PrimitiveDateTime};
 use crate::core::schedule::prepare_entries::prepare_entries;
-use crate::input::schedule::{Schedule, ScheduleEntry, TimeFix};
+use crate::input::schedule::{Schedule, TimeFix};
+use time::{Duration, PrimitiveDateTime};
 use zusi_xml_lib::xml::zusi::zug::fahrplan_eintrag::FahrplanEintrag;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -29,61 +29,74 @@ impl ApplyScheduleState {
 }
 
 pub fn apply(fahrplan_eintraege: &mut Vec<FahrplanEintrag>, schedule: &Schedule) -> Result<(), ApplyScheduleError> {
-    prepare_entries(fahrplan_eintraege, schedule).into_iter().try_fold(
+    let mut prepared_entries = prepare_entries(fahrplan_eintraege, schedule);
+
+    let ApplyScheduleState { time_fix_diff, .. } = prepared_entries.iter_mut().try_fold(
         ApplyScheduleState::new(),
         |mut state, (fahrplan_eintrag, schedule_entry)| {
-            let abfahrt = fahrplan_eintrag.abfahrt.unwrap(); // fahrplan_eintrag.abfahrt.unwrap() is always Some since prepare_entries only returns those entries
-            let (ankunft, is_stop) = match fahrplan_eintrag.ankunft {
-                None => (abfahrt, false),
-                Some(ankunft) => (ankunft, true),
-            };
-            let stop_time = match (schedule_entry, is_stop) {
-                (Some(ScheduleEntry { stop_time: Some(stop_time), .. }), true) => *stop_time,
-                (Some(ScheduleEntry { stop_time: Some(_), .. }), false) =>
-                    return Err(ApplyScheduleError::CannotApplyStopTime { betriebsstelle: fahrplan_eintrag.betriebsstelle.clone() }),
-                (_, _) => abfahrt - ankunft,
-            };
-            let new_ankunft = match state.previous_abfahrt {
-                None => ankunft,
-                Some(abfahrt) => schedule_entry
-                    .map(|entry| abfahrt + entry.driving_time)
-                    .unwrap_or(ankunft + state.previous_abfahrt_change),
-            };
-            let new_abfahrt = new_ankunft + stop_time;
+            match schedule_entry.take() {
+                None => {
+                    fahrplan_eintrag.ankunft = fahrplan_eintrag.ankunft.map(|ankunft| ankunft + state.previous_abfahrt_change);
+                    fahrplan_eintrag.abfahrt = fahrplan_eintrag.abfahrt.map(|abfahrt| abfahrt + state.previous_abfahrt_change);
 
-            if is_stop {
-                fahrplan_eintrag.ankunft = Some(new_ankunft);
-            }
-            fahrplan_eintrag.abfahrt = Some(new_abfahrt);
+                    state.previous_abfahrt = None;
+                    state.previous_abfahrt_change = Duration::seconds(0);
+                }
+                Some(schedule_entry) => {
+                    let abfahrt = fahrplan_eintrag.abfahrt.unwrap(); // fahrplan_eintrag.abfahrt.unwrap() is always Some since prepare_entries only returns those entries
+                    let (ankunft, is_stop) = match fahrplan_eintrag.ankunft {
+                        None => (abfahrt, false),
+                        Some(ankunft) => (ankunft, true),
+                    };
+                    let stop_time = match (schedule_entry.stop_time, is_stop) {
+                        (Some(stop_time), true) => stop_time,
+                        (Some(_), false) =>
+                            return Err(ApplyScheduleError::CannotApplyStopTime { betriebsstelle: fahrplan_eintrag.betriebsstelle.clone() }),
+                        (None, _) => abfahrt - ankunft,
+                    };
+                    let new_ankunft = match state.previous_abfahrt {
+                        None => ankunft,
+                        Some(abfahrt) => abfahrt + schedule_entry.driving_time,
+                    };
+                    let new_abfahrt = new_ankunft + stop_time;
 
-            if schedule_entry.is_some() {
-                state.previous_abfahrt = Some(new_abfahrt);
-                state.previous_abfahrt_change = new_abfahrt - abfahrt;
-            } else {
-                state.previous_abfahrt = None;
-                state.previous_abfahrt_change = Duration::seconds(0);
-            }
+                    if is_stop {
+                        fahrplan_eintrag.ankunft = Some(new_ankunft);
+                    }
+                    fahrplan_eintrag.abfahrt = Some(new_abfahrt);
 
-            match (schedule_entry, state.time_fix_diff) {
-                (Some(ScheduleEntry { time_fix: Some(TimeFix::Ankunft), .. }), None) => state.time_fix_diff = Some(ankunft - new_ankunft),
-                (Some(ScheduleEntry { time_fix: Some(TimeFix::Abfahrt), .. }), None) => state.time_fix_diff = Some(abfahrt - new_abfahrt),
-                (Some(ScheduleEntry { time_fix: Some(_), .. }), Some(_)) => return Err(ApplyScheduleError::TimeFixIsAllowedOnlyOnce),
-                (_, _) => {}
+                    state.previous_abfahrt = Some(new_abfahrt);
+                    state.previous_abfahrt_change = new_abfahrt - abfahrt;
+
+                    match (&schedule_entry.time_fix, state.time_fix_diff) {
+                        (Some(TimeFix::Ankunft), None) if ankunft != new_ankunft => state.time_fix_diff = Some(ankunft - new_ankunft),
+                        (Some(TimeFix::Abfahrt), None) if abfahrt != new_abfahrt => state.time_fix_diff = Some(abfahrt - new_abfahrt),
+                        (Some(_), Some(_)) => return Err(ApplyScheduleError::TimeFixIsAllowedOnlyOnce),
+                        (_, _) => {}
+                    }
+                }
             }
 
             Ok(state)
         }
     )?;
 
+    if let Some(time_fix_diff) = time_fix_diff {
+        for (fahrplan_eintrag, _) in prepared_entries {
+            fahrplan_eintrag.ankunft = fahrplan_eintrag.ankunft.map(|ankunft| ankunft + time_fix_diff);
+            fahrplan_eintrag.abfahrt = fahrplan_eintrag.abfahrt.map(|abfahrt| abfahrt + time_fix_diff);
+        }
+    }
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use time::Duration;
-    use time::macros::datetime;
-    use crate::input::schedule::ScheduleEntry;
     use super::*;
+    use crate::input::schedule::ScheduleEntry;
+    use time::macros::datetime;
+    use time::Duration;
 
     #[test]
     fn test_apply() {
@@ -122,7 +135,7 @@ mod tests {
                     betriebsstelle: "D".into(),
                     driving_time: Duration::minutes(5),
                     stop_time: None,
-                    time_fix: None,
+                    time_fix: Some(TimeFix::Ankunft),
                 },
                 ScheduleEntry {
                     betriebsstelle: "E".into(),
@@ -149,19 +162,19 @@ mod tests {
 
         assert_eq!(modified, vec![
             FahrplanEintrag::builder().betriebsstelle("A".into()).build(),
-            FahrplanEintrag::builder().betriebsstelle("A".into()).abfahrt(Some(datetime!(2022-07-29 04:03:00))).build(),
+            FahrplanEintrag::builder().betriebsstelle("A".into()).abfahrt(Some(datetime!(2022-07-29 04:00:40))).build(),
             FahrplanEintrag::builder().betriebsstelle("B".into()).build(),
-            FahrplanEintrag::builder().betriebsstelle("B".into()).abfahrt(Some(datetime!(2022-07-29 04:06:00))).build(),
+            FahrplanEintrag::builder().betriebsstelle("B".into()).abfahrt(Some(datetime!(2022-07-29 04:03:40))).build(),
             FahrplanEintrag::builder().betriebsstelle("C".into()).build(),
-            FahrplanEintrag::builder().betriebsstelle("C".into()).ankunft(Some(datetime!(2022-07-29 04:09:00))).abfahrt(Some(datetime!(2022-07-29 04:09:20))).build(),
+            FahrplanEintrag::builder().betriebsstelle("C".into()).ankunft(Some(datetime!(2022-07-29 04:06:40))).abfahrt(Some(datetime!(2022-07-29 04:07:00))).build(),
             FahrplanEintrag::builder().betriebsstelle("D".into()).build(),
-            FahrplanEintrag::builder().betriebsstelle("D".into()).ankunft(Some(datetime!(2022-07-29 04:14:20))).abfahrt(Some(datetime!(2022-07-29 04:15:20))).build(),
+            FahrplanEintrag::builder().betriebsstelle("D".into()).ankunft(Some(datetime!(2022-07-29 04:12:00))).abfahrt(Some(datetime!(2022-07-29 04:13:00))).build(),
             FahrplanEintrag::builder().betriebsstelle("E".into()).build(),
-            FahrplanEintrag::builder().betriebsstelle("E".into()).abfahrt(Some(datetime!(2022-07-29 04:18:20))).build(),
+            FahrplanEintrag::builder().betriebsstelle("E".into()).abfahrt(Some(datetime!(2022-07-29 04:16:00))).build(),
             FahrplanEintrag::builder().betriebsstelle("F".into()).build(),
-            FahrplanEintrag::builder().betriebsstelle("F".into()).ankunft(Some(datetime!(2022-07-29 04:22:20))).abfahrt(Some(datetime!(2022-07-29 04:23:00))).build(),
+            FahrplanEintrag::builder().betriebsstelle("F".into()).ankunft(Some(datetime!(2022-07-29 04:20:00))).abfahrt(Some(datetime!(2022-07-29 04:20:40))).build(),
             FahrplanEintrag::builder().betriebsstelle("G".into()).build(),
-            FahrplanEintrag::builder().betriebsstelle("G".into()).abfahrt(Some(datetime!(2022-07-29 04:25:00))).build(),
+            FahrplanEintrag::builder().betriebsstelle("G".into()).abfahrt(Some(datetime!(2022-07-29 04:22:40))).build(),
         ]);
     }
 }
