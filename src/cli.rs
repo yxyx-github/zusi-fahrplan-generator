c0f81dbe974bb29404a6dace4376821a238f2bdb
yxyx-github <dev_mw@vodafonemail.de>
2025-09-12 10:35:46 +0200

    implement cli for schedule (apply and generate)

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,5 @@
-use std::path::PathBuf;
 use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -11,12 +11,45 @@ pub struct Cli {
 #[derive(Subcommand)]
 pub enum CliCommand {
     GenerateFahrplan(GenerateFahrplanArgs),
+
+    #[command(subcommand)]
+    Schedule(CliScheduleCommand),
 }
 
-#[derive(Args, Debug)]
+#[derive(Subcommand)]
+pub enum CliScheduleCommand {
+    Apply(ApplyScheduleArgs),
+    Generate(GenerateScheduleArgs),
+}
+
 /// Copy trains and delay them by given time
+#[derive(Args, Debug)]
 pub struct GenerateFahrplanArgs {
     /// Path to config file
     #[arg(short, long)]
     pub config: PathBuf,
+}
+
+/// Updates times in specified .trn files according to provided schedule file
+#[derive(Args, Debug)]
+pub struct ApplyScheduleArgs {
+    /// Path to schedule file
+    #[arg(short, long)]
+    pub schedule: PathBuf,
+
+    /// .trn files to modify
+    #[arg(short, long, num_args = 1..)]
+    pub trn_files: Vec<PathBuf>,
+}
+
+/// Generates a schedule file based on the given .trn file
+#[derive(Args, Debug)]
+pub struct GenerateScheduleArgs {
+    /// Path to trn file
+    #[arg(short, long)]
+    pub trn: String,
+
+    /// Path where to create the schedule file
+    #[arg(short, long)]
+    pub schedule: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use clap::Parser;
-use serde_helpers::xml::FromXML;
-use zusi_fahrplan_generator::cli::{Cli, CliCommand};
+use serde_helpers::xml::{FromXML, ToXML};
+use std::path::Path;
+use zusi_fahrplan_generator::cli::{Cli, CliCommand, CliScheduleCommand};
 use zusi_fahrplan_generator::core::generate_fahrplan::generate_fahrplan;
+use zusi_fahrplan_generator::core::lib::helpers::read_zug;
+use zusi_fahrplan_generator::core::schedules::apply::apply_schedule;
 use zusi_fahrplan_generator::input::environment::zusi_environment_config::ZusiEnvironmentConfig;
 use zusi_fahrplan_generator::input::fahrplan_config::FahrplanConfig;
+use zusi_fahrplan_generator::input::schedule::Schedule;
+use zusi_xml_lib::xml::zusi::Zusi;
+use zusi_fahrplan_generator::core::schedules::generate::generate_schedule;
 
 fn main() -> Result<(), String> {
     let cli = Cli::parse();
@@ -11,6 +17,7 @@ fn main() -> Result<(), String> {
     match cli.command {
         CliCommand::GenerateFahrplan(args) => {
             let config_path = args.config;
+
             println!(r#"Generate Fahrplan using config file at "{}""#, config_path.display());
             let config = ZusiEnvironmentConfig::<FahrplanConfig>::from_xml_file_by_path(&config_path)
                 .map_err(|error| format!("Couldn't read the config file: {error}"))?;
@@ -18,6 +25,41 @@ fn main() -> Result<(), String> {
                 .map_err(|error| format!("Couldn't create the ZusiEnvironment: {error}"))?;
             println!("{environment}");
             generate_fahrplan(&environment, fahrplan_config).map_err(|error| format!("{error}"))
+        },
+        CliCommand::Schedule(CliScheduleCommand::Apply(args)) => {
+            let schedule_path = args.schedule;
+            let trn_file_paths = args.trn_files;
+
+            let schedule = Schedule::from_xml_file_by_path(schedule_path)
+                .map_err(|error| format!("Couldn't read the schedule file: {error}"))?;
+            trn_file_paths
+                .into_iter()
+                .for_each(|trn_file_path| {
+                    apply_schedule_to_file(&schedule, &trn_file_path)
+                        .unwrap_or_else(|error| eprintln!(r#"Error occoured for "{}": {error}"#, trn_file_path.display()));
+                });
+            Ok(())
+        },
+        CliCommand::Schedule(CliScheduleCommand::Generate(args)) => {
+            let schedule_path = args.schedule;
+            let trn_file_path = args.trn;
+
+            let zug = read_zug(trn_file_path)
+                .map_err(|error| format!(r"Couldn't read the trn file: {error}"))?;
+            let schedule = generate_schedule(&zug.value.fahrplan_eintraege);
+            schedule.to_xml_file_by_path(schedule_path, true).map_err(|error| format!(r"Couldn't write the schedule file: {error}"))
         }
     }
+}
+
+fn apply_schedule_to_file<P: AsRef<Path>>(schedule: &Schedule, trn_file_path: P) -> Result<(), String> {
+    let trn_file_path = trn_file_path.as_ref();
+    let mut zug = read_zug(trn_file_path)
+        .map_err(|error| format!(r"Couldn't read the trn file: {error}"))?;
+    apply_schedule(&mut zug.value.fahrplan_eintraege, schedule)
+        .map_err(|error| format!(r"Couldn't apply the schedule: {error}"))?;
+    let zug: Zusi = zug.into();
+    zug.to_xml_file_by_path(trn_file_path, true)
+        .map_err(|error| format!(r"Couldn't write the trn file: {error}"))?;
+    Ok(())
 }
